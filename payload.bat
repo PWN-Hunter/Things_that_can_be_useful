95ce5f663e2c6a8f41450f5c6175827f3dfb1e1f
Martin Valba <kailing2@live.com>
2020-11-07 17:20:39 +0200

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -2,8 +2,3 @@
 clear
 echo "This script could been a real payload and it executed as needed here"
 pause
-echo If u want me to clean the payload dir hit enter. But if not then close me at the big red X!
-pause
-cd / && rmdir /s /q payload
-echo done, hit enter to close me
-pause
