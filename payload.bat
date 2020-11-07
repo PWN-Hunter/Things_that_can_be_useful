c5ab22b3285e803cb1db59c9e8a247e208704275
Martin Valba <kailing2@live.com>
2020-11-07 17:27:19 +0200

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
 echo "This script could been a real payload and it executed as needed here"
-echo 
 echo the leftovers are in C:\payload so feel free to remove these.
+echo also the payloads source code is here: https://github.com/PWN-Hunter/Things_that_can_be_useful
 pause
